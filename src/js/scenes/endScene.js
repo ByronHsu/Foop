8517873269fd8a947a765a977ebb1842f744e178
csiejoey <asdfasdfasdf12tw@gmail.com>
2018-02-20 17:57:13 +0100

    :sparkles::hankey: add world rank

--- src/js/scenes/endScene.js
+++ src/js/scenes/endScene.js
@@ -35,24 +35,42 @@ function showEndScene(player) {
     }
   }
   // show Texts
-  let text = new Text(
-    `${JSON.parse(localStorage.getItem('user')).name}\nThis Game Money: ${
-      player.money
-    }\nThis Game Score: ${
-      player.exitTimes
-    }\nTotal Money: ${totalMoney}\nHighest Score: ${recordScore}`,
-    { fontFamily: 'Orbitron-Medium, sans-serif' }
-  );
-  text.position.set(0, config.app.h / 2);
-  endCtn.addChild(text);
-  // post player name and game data to api
+  // let text = new Text(
+  //   `${JSON.parse(localStorage.getItem('user')).name}\nThis Game Money: ${
+  //     player.money
+  //   }\nThis Game Score: ${
+  //     player.exitTimes
+  //   }\nTotal Money: ${totalMoney}\nHighest Score: ${recordScore}`,
+  //   { fontFamily: 'Orbitron-Medium, sans-serif' }
+  // );
+  // text.position.set(0, config.app.h / 2);
+  // endCtn.addChild(text);
+
+  // Show World Rank:
   axios
     .post('/api/data', {
       name: JSON.parse(localStorage.getItem('user')).name,
       score: player.exitTimes,
     })
-    .then(axios.get('/api/data').then(res => console.log(res.data))) // see all stored data
+    .then(
+      axios.get('/api/data').then(res => {
+        let rankData = res.data;
+        let text = new Text('World Rank', {
+          fontFamily: 'Orbitron-Medium, sans-serif',
+        });
+        text.position.set(config.ww / 2, 0);
+        endCtn.addChild(text);
+        for (let i = 0; i < rankData.length; i++) {
+          let text = new Text(`${rankData[i].name}: ${rankData[i].score}`, {
+            fontFamily: 'Orbitron-Medium, sans-serif',
+          });
+          text.position.set(config.ww / 2, i * 50 + 50);
+          endCtn.addChild(text);
+        }
+      })
+    )
     .catch(err => console.error(err));
+
   // setup restartBtn
   setupRestart.call(this);
   endCtn.addChild(this.restart);
